0f1f2f98b7b701d92d329548596b5cb8f1ae4deb
alce65 <alce65@hotmail.es>
2017-10-30 12:35:30 +0100

    Comunicaciones

---
+++ 04_comunicacion/comunics/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { SharedModule } from './shared/shared.module';
+import { ComponentesModule } from './componentes/componentes.module';
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ComponentesModule,
+    SharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
