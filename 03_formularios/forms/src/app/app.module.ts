8120af723709a5993ac19a1ed8215b33d5a5989c
alce65 <alce65@hotmail.es>
2017-10-30 11:14:07 +0100

    Formularios y validacion

--- 03_formularios/forms/src/app/app.module.ts
+++ 03_formularios/forms/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
 import { Form1Component } from './form1/form1.component';
+import { Form2Component } from './form2/form2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Form1Component
+    Form1Component,
+    Form2Component
   ],
   imports: [
     BrowserModule,
--- 03_formularios/forms/src/app/form2/form2.component.ts
+++ 03_formularios/forms/src/app/form2/form2.component.ts
@@ -1,14 +1,14 @@
-import { Usuario, Impresora, Departamento } from './form1.model';
-import { Component, OnInit } from '@angular/core';
-
+import { Usuario, Impresora, Departamento } from './form.model';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'app-form1',
-  templateUrl: './form1.component.html',
-  styleUrls: ['./form1.component.css']
+  selector: 'app-form2',
+  templateUrl: './form2.component.html',
+  styleUrls: ['./form2.component.css']
 })
-export class Form1Component implements OnInit {
+export class Form2Component implements OnInit {
 
+  @ViewChild ('myform') formulario: any;
   user: Usuario;
   impresoraSelect: Impresora;
   aImpresoras: Array<Impresora>;
@@ -20,6 +20,7 @@ export class Form1Component implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
     this.user = {nombre: '', apellidos: '',  telefono: ''};
     this.aImpresoras = [
       {nombre: 'HP', codigo: '001'},
@@ -33,10 +34,19 @@ export class Form1Component implements OnInit {
       {nombre: 'Finanzas', codigo: 'FNZ'},
       {nombre: 'Sistenas', codigo: 'SST'}
     ];
+
+    console.log(this.formulario);
   }
 
-  enviar() {}
+  enviar() {
+    console.log('Datos enviados');
+  }
 
-  borrar() {}
+  borrar() {
+    this.formulario.reset();
+  }
 
+  avisarPrint() {}
+
+  avisarDepartamento() {}
 }
